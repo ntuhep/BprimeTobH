f0ed4e957a0ec8f469e67a5dbb87cf63b4b827e8
Devdatta <devdatta.majumder@cern.ch>
2013-11-27 21:52:14 +0100

    Many debugs

--- python/JetSelector_cfi.py
+++ python/JetSelector_cfi.py
@@ -2,11 +2,11 @@ import FWCore.ParameterSet.Config as cms
 
 defaultJetSelectionParameters = cms.PSet(
     jettype             = cms.string('AK5JET'),
-    jetPtMin            = cms.double(30),
+    jetPtMin            = cms.double(50),
     jetPtMax            = cms.double(100000),
+    jetAbsEtaMax        = cms.double(2.4),
     jetCSVDiscMin       = cms.double(0.000),   
     jetCSVDiscMax       = cms.double(1.000),   
-    jetAbsEtaMax        = cms.double(2.4),
     IsJetIDLoose        = cms.bool(True), 
     IsJetIDTight        = cms.bool(False), 
     )
