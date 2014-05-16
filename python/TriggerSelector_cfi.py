fc6e987cac53e54390465e3299fb7720b7b1098a
eleni <petrakou@cern.ch>
2014-05-16 15:06:25 +0200

    HLT efficiency

--- python/TriggerSelector_cfi.py
+++ python/TriggerSelector_cfi.py
@@ -4,6 +4,14 @@ HLTPathNames = [
     'HLT_HT750'
     ]
 
+HLTMuPathNames = [
+    'HLT_Mu22'
+    ]
+    
 defaultTriggerSelectionParameters = cms.PSet(
     HLTPaths = cms.vstring(HLTPathNames) 
+    )
+    
+defaultMuTriggerSelectionParameters = cms.PSet(
+    HLTPaths = cms.vstring(HLTMuPathNames) 
     )
